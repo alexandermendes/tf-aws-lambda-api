da80a317484e0829d266113193defc746f8523dd
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-13 22:03:09 +0100

    fix: lambda role ID

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "invoke_url" {
-  value       = "${module.api.invoke_url}"
+  value       = module.api.invoke_url
   description = "The URL to invoke the Lambda function."
 }
 
-output "role_id" {
-  value       = module.api.role_id
+output "lambda_role_id" {
+  value       = module.lambda.role_id
   description = "The ID of the role used for the Lambda function."
 }
